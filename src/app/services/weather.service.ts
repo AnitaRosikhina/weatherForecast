eb9cf19c5056d767c7bdd31a6d8ec9a007ec8079
Anita Rosikhina <anita.rosikhina@hebronsoft.com>
2023-06-21 15:49:13 +0300

    updated app.component

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) {}
+
+  getWeatherByCity(): Observable<any>{
+    return this.httpClient.get(`https://api.weatherapi.com/v12ca7f9dad835472284c94348232106`)
+  }
 }
